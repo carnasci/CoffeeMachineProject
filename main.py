209a5f7b0ac3535c627fd9f7b3d452d210271969
carnasci <cen.master24@gmail.com>
2024-12-18 14:38:34 -0600

    First 4 TODO's completed - so far machine can check if there are enough resources to make the drink

--- main.py
+++ main.py
@@ -47,9 +47,22 @@ def generate_report(machine_resources):
         else:
             print(f"{key.title()}: ${value}")
 
+def check_if_resources_sufficient(drink, machine_resources):
+    """Takes required resources and current resources and checks if drink is able to be made."""
+    for key, value in drink.items():
+        for k, v in machine_resources.items():
+            if key == k and v > value:
+                return True
+            elif key == k and v < value:
+                print(f"Sorry, not enough {key}.")
+                return False
+
 # TODO#1 prompt user by asking "What would you like (espresso/latte/cappuccino)?"
 # TODO#2 turn the machine off when given the prompt "off"
 # TODO#3 generates a report of current resources when given the prompt "report"
+# TODO#4 check if resources are sufficient
+# TODO#5 if there is enough resources to make a drink ask the user for coins
+
 while on:
     user_choice = input("What would you like (espresso/latte/cappuccino)? ").lower()
 
@@ -57,3 +70,8 @@ while on:
         on = False
     elif user_choice == "report":
         generate_report(current_resources)
+    elif user_choice in MENU:
+        user_choice = MENU[user_choice]
+        print(user_choice)
+
+    print(check_if_resources_sufficient(user_choice["ingredients"], current_resources))
